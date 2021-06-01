7948bce348a0d02c8a1d21260421d3e0515b9c51
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-01 10:08:22 -0400

    Serve JSON to GET requests to /json route.

--- myApp.js
+++ myApp.js
@@ -11,6 +11,10 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"});
+})
+
 console.log("Hello World");
 
 
