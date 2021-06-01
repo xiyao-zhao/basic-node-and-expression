a36d55e6c60591bc79b97ddbf0c2704f56e6d40b
Anna Zhao <48630237+631536288@users.noreply.github.com>
2021-06-01 09:47:30 -0400

    Send an html file as a response to GET requests to the / path.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function (req, res) {
+/*app.get("/", function(req, res) {
     res.send("Hello Express");
+})*/
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 })
 
 console.log("Hello World");
